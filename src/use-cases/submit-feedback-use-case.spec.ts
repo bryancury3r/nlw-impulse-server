08ee3641f8abc307712b8ce600e234129eb2760c
bryancury3r <gabriel_bryancury@outlook.com>
2022-05-29 23:10:06 -0300

    text unitario

--- src/use-cases/submit-feedback-use-case.spec.ts
+++ src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,3 +1,42 @@
-test('sum 2 + 2', () => {
-  expect(2 + 2).toBe(4)
+import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
+
+const createFeedbacSpy = jest.fn();
+
+const submitFeedback = new SubmitFeedbackUseCase(
+  { create: async () => {}},
+  { sendMail: async () => {} }
+)
+
+describe('Submit feedback', () => {
+  it('should be able to submit a feedback', async () => {
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: 'Example comment',
+      screenshot: 'data:image/png;base64,1fs5d1f65sdf6sdf6',
+    })).resolves.not.toThrow();
+  });
+
+  it('should not be able to submit a feedback without type', async () => {
+    await expect(submitFeedback.execute({
+      type: '',
+      comment: 'Example comment',
+      screenshot: 'data:image/png;base64,1fs5d1f65sdf6sdf6',
+    })).rejects.toThrow();
+  });
+
+  it('should not be able to submit a feedback without comment', async () => {
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: '',
+      screenshot: 'data:image/png;base64,1fs5d1f65sdf6sdf6',
+    })).rejects.toThrow();
+  });
+
+  it('should not be able to submit a feedback with an invalid screenshot', async () => {
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: 'Tá tudo bugado',
+      screenshot: 'test.jpg',
+    })).rejects.toThrow();
+  });
 });
